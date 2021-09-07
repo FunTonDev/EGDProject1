f27f653d0ba7d7c022cd960e241db2259feb03f2
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2021-09-07 15:11:39 -0400

    Added refined platformer movement to player.
    Added horizontal movement + Jump
    Made player character prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,33 +8,54 @@ public class PlayerController : MonoBehaviour
      * ALL THIS IS STARTER/SCRATCH CODE
      * USE/CHANGE/DELETE AS YOU WISH
      */
+     //Rigid body of the player character
     private Rigidbody2D playerRB;
-    private float playerSpeed, playerDirection;
+    //Speed that the player moves at horizontally
+    public float playerSpeed;
+    //Force the player jumps up with
+    public float jumpForce;
+    private float playerDirection;
+    //Whether the player is on the ground or not
     private bool grounded;
 
     private void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
-        playerSpeed = 5.0f;
+        //playerSpeed = 5.0f;
         playerDirection = 1.0f;
+        grounded = true;
     }
 
     void FixedUpdate()
     {
-        playerRB.velocity = new Vector2(playerSpeed * playerDirection, 0);        
+        //Get movement input in the horizontal axis, multiply by speed, and move that amount
+        float mover = Input.GetAxisRaw("Horizontal");
+        mover = mover * playerSpeed;
+        playerRB.velocity = new Vector2(mover, playerRB.velocity.y);
+        //playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * playerSpeed;
     }
 
     private void Update()
     {
-        playerDirection = Input.GetKeyDown(KeyCode.LeftArrow) ? -1.0f : playerDirection;
-        playerDirection = Input.GetKeyDown(KeyCode.RightArrow) ? 1.0f : playerDirection;
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        //playerDirection = (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) ? -1.0f : playerDirection;
+        //playerDirection = (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) ? 1.0f : playerDirection;
+
+        //JUMP
+        if (Input.GetAxisRaw("Vertical") > 0 && grounded)
         {
-            //JUMP
+            playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+            grounded = false;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        //INTERACT
+        if (Input.GetAxisRaw("Vertical") < 0)
         {
             //INTERACT
+            Debug.Log("Should interact");
+        }
+        if (playerRB.velocity.y == 0)
+        {
+            grounded = true;
         }
     }
 }
