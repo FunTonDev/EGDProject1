c2d119d5088a18ab041a0ff051837c9185f5fa09
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2021-09-08 00:12:13 -0400

    Added basic crop functionality and stage manager can randomly assign crops to plots

--- Assets/Crop.cs
+++ Assets/Crop.cs
@@ -1,25 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Crop : MonoBehaviour
 {
+    //The amount of time left to water the plot
     public float waterTimer;
+    //Max amount of time
+    public float waterTimerMax;
+    //Whether the plot has a crop or not
     public bool hasCrop;
+    //The image tied to the plot of land
+    public SpriteRenderer plot;
+    //A bar to show how much time is left to water the crop
+    public Image waterBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        waterTimerMax = 5;
+        waterTimer = 5;
+    }
+
+    //Give the plot of land a crop
+    public void getCrop()
+    {
+        hasCrop = true;
+        plot.color = new Color(0.0f, 1.0f, 0.0f);
+        waterTimer = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
         waterTimer -= Time.deltaTime;
-        if (waterTimer <= 0)
+        waterBar.fillAmount = waterTimer / waterTimerMax;
+        if (waterTimer <= 0 && hasCrop)
         {
-
+            hasCrop = false;
+            plot.color = new Color(1.0f, 0.5f, 0.0f);
         }
     }
 }
--- Assets/StageManager.cs
+++ Assets/StageManager.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
+    //A grouping of all plots on the stage
     public GameObject[] plots;
+    //The current score 
     public int score;
 
     // Start is called before the first frame update
     void Start()
     {
         plots = GameObject.FindGameObjectsWithTag("Crop");
-        for (int i = 0; i < plots.Length; i++)
+        if (plots.Length > 1)
         {
-            int rando = Random.RandomRange(0, 5);
+            for (int i = 0; i < plots.Length; i++)
+            {
+                int rando = Random.Range(0, 5);
+                if (rando <= 1)
+                {
+                    plots[i].GetComponent<Crop>().getCrop();
+                }
+            }
+        }
+        else
+        {
+            plots[0].GetComponent<Crop>().getCrop();
         }
     }
 
