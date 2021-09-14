41a037130a1e0995322deb3a3ecbfc6dda0525a0
Jon Lafuente <jonlafuente99@gmail.com>
2021-09-14 18:43:25 -0400

    Added dialogue to intro scene

--- Assets/Scripts/CinematicManager.cs
+++ Assets/Scripts/CinematicManager.cs
@@ -14,9 +14,11 @@ public class CinematicManager : MonoBehaviour
     public List<Sprite> end2Sprites;
     public GameObject continueButton;
     public GameObject gameGuide;
+    public Text introText;
 
     void Start()
     {
+        introText.color = new Color(introText.color.r, introText.color.g, introText.color.b, 0);
         gameGuide.SetActive(false);
         continueButton.SetActive(false);
         switch(GameManager.cinematicChosen)
@@ -91,6 +93,34 @@ public class CinematicManager : MonoBehaviour
         auxSource.PlayOneShot(tClip);
     }
 
+    private void TextSwitch(int part)
+    {
+        switch (part)
+        {
+            case 1:
+                introText.text = "Our beautiful planet, our home, is dying.";
+                break;
+            case 2:
+                introText.text = "After many years of overpopulation and depleting earth of its natural resources, the nations of the world have decided to come together to form a solution to this problem.";
+                break;
+            case 3:
+                introText.text = "The answer: the creation of a space botanical organization, dubbed Earth’s Natural Development Team(END).";
+                break;
+            case 4:
+                introText.text = "END searched far and wide, until they found their answer in South Dakota.";
+                break;
+            case 5:
+                introText.text = "They enlist the help of you, the Gardener, to grow plants in efforts to save the world.";
+                break;
+            case 6:
+                introText.text = "No pressure, right? Of course not; if the earth perishes then we just go to Mars and hope history doesn't repeat itself.";
+                break;
+            case 7:
+                introText.text = "If all else fails, abandon ship, but do you really want to?";
+                break;
+        }
+    }
+
     private IEnumerator AnimationLoop(List<Sprite> animationSprites)
     {
         for(int index = 0;;index++)
@@ -106,7 +136,21 @@ public class CinematicManager : MonoBehaviour
 
     private IEnumerator IntroTextSequence()
     {
-
+        yield return new WaitForSeconds(1.0f);
+        introText.color = new Color(1, 1, 1, 1);
+        TextSwitch(1);
+        yield return new WaitForSeconds(5.0f);
+        TextSwitch(2);
+        yield return new WaitForSeconds(10.0f);
+        TextSwitch(3);
+        yield return new WaitForSeconds(7.0f);
+        TextSwitch(4);
+        yield return new WaitForSeconds(6.0f);
+        TextSwitch(5);
+        yield return new WaitForSeconds(6.0f);
+        TextSwitch(6);
+        yield return new WaitForSeconds(7.0f);
+        TextSwitch(7);
         yield return StartCoroutine(CinematicEndBuffer(2.0f));
     }
 
